f0bce35037bc20f2fbcb4f2f2437f44b6781d2dd
Kay-Conte <levsucks584@outlook.com>
2023-10-03 16:50:07 -0400

    [feat] provide a mechanism to initialize a typecache

--- src/framework.rs
+++ src/framework.rs
@@ -7,18 +7,27 @@ use std::{
 
 use crate::{
     routing::Route,
-    tasks::{ConnectionTask, TaskPool, RequestTask}, type_cache::TypeCache, 
+    tasks::{ConnectionTask, RequestTask, TaskPool},
+    type_cache::TypeCache,
 };
 
-/// Application entry point. Call this function to run your application.
+/// Application entry point. Call this to run your application.
 pub fn run<A>(address: A, router: Route)
+where
+    A: ToSocketAddrs,
+{
+    run_with_cache(address, router, TypeCache::new())
+}
+
+/// Application entry point with an initialized cache.
+pub fn run_with_cache<A>(address: A, router: Route, type_cache: TypeCache)
 where
     A: ToSocketAddrs,
 {
     let incoming = TcpListener::bind(address).expect("Could not bind to local address");
 
     let router = Arc::new(router);
-    let type_cache = Arc::new(RwLock::new(TypeCache::new()));
+    let type_cache = Arc::new(RwLock::new(type_cache));
 
     let connection_pool = TaskPool::<ConnectionTask>::default();
     let request_pool = Arc::new(TaskPool::<RequestTask>::default());
