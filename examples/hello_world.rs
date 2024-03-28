fcf5aae63c432481ba43273c7f639b33d497cae1
Kay-Conte <levsucks584@outlook.com>
2024-03-28 14:01:30 -0400

    chore(examples): added multiple methods example to hello_world

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,11 +1,20 @@
-use foxhole::{action::Html, App, Http1, Method::Get, Router};
+use foxhole::{
+    action::Html,
+    App, Http1,
+    Method::{Get, Post},
+    Router,
+};
 
 fn get() -> Html {
     Html(String::from("<h1> Foxhole! </h1>"))
 }
 
+fn post() -> u16 {
+    200
+}
+
 fn main() {
-    let scope = Router::new().add_route("/", Get(get));
+    let scope = Router::new().add_route("/", (Get(get), Post(post)));
 
     println!("Running on '127.0.0.1:8080'");
 
