64ab34adebd12dded94d8af2726855042f0697d7
Kay-Conte <levsucks584@outlook.com>
2024-03-29 12:34:43 -0400

    feat(example): replay url_pieces example with a more concise example

---
+++ examples/url_parameters.rs
@@ -0,0 +1,31 @@
+use foxhole::{
+    action::Html,
+    resolve::{ArgMap, UrlCollect, UrlPart},
+    App, Http1,
+    Method::Get,
+    Router,
+};
+
+fn user(UrlPart(part): UrlPart) -> Html {
+    Html(part)
+}
+
+fn collect(UrlCollect(after): UrlCollect) -> Html {
+    Html(after.join(""))
+}
+
+// /query?q=hello
+fn query(ArgMap(map): ArgMap) -> Option<Html> {
+    map.get("q").map(|i| Html(i.to_string()))
+}
+
+fn main() {
+    let router = Router::new()
+        .add_route("/user/:username", Get(user))
+        .add_route("/collect/*", Get(collect))
+        .add_route("/query", Get(query));
+
+    println!("Try connecting on a browser at 'http://localhost:8080/user/USERNAME'");
+
+    App::builder(router).run::<Http1>("0.0.0.0:8080");
+}
